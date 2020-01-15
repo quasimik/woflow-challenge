a1ee3b9c8e728e72600010cd56ae2478d59168f9
Michael Liu <mike666234@gmail.com>
2020-01-15 15:19:01 -0800

    Remove $, whitespaces

--- spider.py
+++ spider.py
@@ -1,7 +1,7 @@
 import scrapy
 
 class WoflowChallengeSpider(scrapy.Spider):
-  name = 'woflow'
+  name = 'woflow-challenge'
   start_urls = [
     'https://menupages.com/asian-cajun-too/1322-chicago-ave-evanston',
   ]
@@ -12,12 +12,23 @@ class WoflowChallengeSpider(scrapy.Spider):
     for category in response.css('div.menu-section'):
       category_name = category.css('div h3::text').get().strip()
       category_items = []
-      # category.css('div.menu-item')[0]
       for item in category.css('div.menu-item'):
+        item_name = item.css('a.menu-item__title-link::text').get()
+        item_desc = item.css('p.menu-item__description::text').get()
+        item_price = item.css('span.menu-item__price::text').get().replace('$', '')
+
+        # remove trailing whitespaces
+        if item_name is not None:
+          item_name = item_name.strip()
+        if item_desc is not None:
+          item_desc = item_desc.strip()
+        if item_price is not None:
+          item_price = item_price.strip()
+
         category_items.append({
-          'name': item.css('a.menu-item__title-link::text').get(),
-          'description': item.css('p.menu-item__description::text').get(),
-          'price': item.css('span.menu-item__price::text').get(),
+          'name': item_name,
+          'description': item_desc,
+          'price': item_price,
         })
       restaurant_items.append({
         'name': category_name,
